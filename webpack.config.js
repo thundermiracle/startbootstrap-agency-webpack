8580cbdd79d816dc62b4862ebc39487638ae42fe
thundermiracle <cissoliu@gmail.com>
2020-06-29 17:44:35 +0900

    use HtmlWebpackPlugin instead of CopyWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,54 @@
+// Common Plugins
 const path = require("path");
-const webpack = require("webpack");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const WebpackMerge = require("webpack-merge");
 
-module.exports = {
-  mode: "production",
+// Development Plugins
+const Webpack = require("webpack");
+
+// Production Plugins
+const TerserJSPlugin = require("terser-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+// production or development
+const devMode = !process.argv.includes("--mode=production");
+
+const commonConfig = {
   entry: path.resolve(__dirname, "./src/main.js"),
   output: {
-    filename: "bundle.js",
+    filename: "[name].[hash:8].js",
     path: path.resolve(__dirname, "./dist"),
-  },
-  devServer: {
-    contentBase: "./dist",
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: devMode,
+            },
+          },
+          "css-loader",
+          "postcss-loader",
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
       },
       {
         test: /\.scss$/,
         use: [
-          MiniCssExtractPlugin.loader,
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: devMode,
+            },
+          },
           "css-loader",
           "postcss-loader",
           "sass-loader",
@@ -39,7 +64,7 @@ module.exports = {
               fallback: {
                 loader: "file-loader",
                 options: {
-                  outputPath: "img",
+                  name: "./img/[name].[hash:8].[ext]",
                 },
               },
             },
@@ -49,15 +74,41 @@ module.exports = {
     ],
   },
   plugins: [
-    new webpack.ProvidePlugin({
+    new Webpack.ProvidePlugin({
       $: "jquery",
       jQuery: "jquery",
       "window.jQuery": "jquery",
     }),
     new MiniCssExtractPlugin({
-      filename: "style.css",
+      filename: devMode ? "[name].css" : "[name].[hash].css",
+      chunkFilename: devMode ? "[id].css" : "[id].[hash].css",
     }),
-    new CopyWebpackPlugin([{ from: "./public", to: "." }]),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./public/index.html"),
+      minify: false,
+    }),
     new CleanWebpackPlugin(),
   ],
 };
+
+// create config by mode
+const extraConfig = devMode
+  ? {
+      mode: "development",
+      devtool: "cheap-module-eval-source-map",
+      devServer: {
+        port: 3000,
+        hot: true,
+        contentBase: "./dist",
+      },
+      plugins: [new Webpack.HotModuleReplacementPlugin()],
+    }
+  : {
+      mode: "production",
+      devtool: "cheap-module-source-map",
+      optimization: {
+        minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+      },
+    };
+
+module.exports = WebpackMerge(commonConfig, extraConfig);
